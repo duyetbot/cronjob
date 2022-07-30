f00d0197b6cf7c918d032eea22d798f3f00555c8
Duyet Le <lvduyet@fossil.com>
2022-07-30 10:14:33 +0700

    feat: auto invite

---
+++ scripts/auto-invite-duyet.js
@@ -0,0 +1,41 @@
+module.exports = async ({ github, context }) => {
+  const username = 'duyet'
+  const owner = context.repo.owner
+
+  let repos = await github.rest.repos.listForUser({
+    username: owner,
+    type: 'owner',
+    sort: 'created',
+  })
+  console.log('Repos', repos)
+
+  for (let repo of repos.data) {
+    console.log(`Checking repo`, repo)
+
+    try {
+      console.log(`Check if a user is a repository collaborator`)
+      await github.rest.repos.checkCollaborator({
+        owner,
+        repo: repo.name,
+        username,
+      })
+      console.log('ok')
+    } catch (e) {
+      if (e.status != 404) {
+        console.log('Unexpected error, skip', e)
+        continue
+      }
+
+      console.log(`${username} is not a collaborator for ${repo.full_name}`)
+      console.log(`Inviting ${username} to ${repo.full_name} ...`)
+
+      const status = await github.rest.repos.addCollaborator({
+        owner: repo.owner.login,
+        repo: repo.name,
+        username,
+      })
+
+      console.log(`Invited ${status}`)
+    }
+  }
+}
